42e28dbce3dedcbc25ef171a9d2601b158b1af8c
dimp10 <65302354+dimp10@users.noreply.github.com>
2023-07-07 19:15:47 +0100

    Update main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,31 @@
+
 tiles.setCurrentTilemap(tilemap`World`)
 
-
+let mySprite = sprites.create(assets.image`Forage ma n`, SpriteKind.Player)
 
 function generateIslandAssets(): void
 {
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < randint(3, 17); i++) {
         let randCol: number = randint(17, 30);
-        let randRow: number = randint(17, 30);
+        let randRow: number = randint(1, 14);
 
-        let touchGrass: boolean = tiles.tileAtLocationEquals(tiles.getTileLocation(randCol, randRow), img`grass`);
+//        let touchGrass: boolean = tiles.tileAtLocationEquals(tiles.getTileLocation(randCol, randRow), img`islandTile4`);
 
-        if (touchGrass == true) {
-            let randChance: number = randint(0, 10);
-
-            switch (randChance) {
-                case 5 :
-                    tiles.placeOnTile(assets.image`tree`, tiles.getTileLocation(randCol, randRow))
-                    break;
-                case 5:
-                    tiles.placeOnTile(assets.image`tree`, tiles.getTileLocation(randCol, randRow))
-                    break;
-                default:
-                    tiles.placeOnTile(assets.image`tree`, tiles.getTileLocation(randCol, randRow))
-                    break;
-            }
-        }
+//         if (touchGrass == true) {
+//             let randChance: number = randint(0, 10);
+// tiles.placeOnTile(null, tiles.getTileLocation(0, 0))
+//             switch (randChance) {
+//                 case 5 :
+//                     tiles.placeOnTile(assets.image`treee`, tiles.getTileLocation(randCol, randRow));
+//                     break;
+//                 case 5:
+//                     tiles.placeOnTile(assets.image`tree`, tiles.getTileLocation(randCol, randRow))
+//                     break;
+//                 default:
+//                     tiles.placeOnTile(assets.image`tree`, tiles.getTileLocation(randCol, randRow))
+//                     break;
+//             }
+//         }
     }
 }
 
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -2,6 +2,10 @@
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -95,6 +99,10 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
+            case "tree":
+            case "tile1":return tile1;
+            case "islandTile4":
+            case "tile2":return tile2;
         }
         return null;
     })
